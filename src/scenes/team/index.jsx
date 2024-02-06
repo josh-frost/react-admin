91271c8d00ba65cdb5f3758ef066c72fa2c9acc9
josh-frost <frost.joshe@gmail.com>
2024-02-06 07:58:58 -0600

    feat: team page

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -13,10 +13,79 @@ const Team = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "access",
+      headerName: "Acess Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto 0 0"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              access === "admin"
+                ? colors.greenAccent[500]
+                : colors.greenAccent[700]
+            }
+            borderRadius="4px"
+          >
+            {access === "admin" && (
+              <AdminPanelSettingsOutlined
+                sx={{ color: colors.redAccent[500] }}
+              />
+            )}
+            {access === "manager" && <SecurityOutlined />}
+            {access === "user" && <LockOpenOutlined />}
+            <Typography
+              color={
+                access === "admin" ? colors.redAccent[500] : colors.gray[100]
+              }
+              sx={{ ml: "5px" }}
+            >
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+
   return (
-    <Box>
+    <Box m="20px">
       <Header title="TEAM" subtitle="Manage the Team Members" />
-      <Box>
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{ "& .MuiDataGrid-root": { border: "none" } }}
+      >
         <DataGrid rows={mockDataTeam} columns={columns} />
       </Box>
     </Box>
